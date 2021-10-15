b704e012bf15fb9a16d2365f288fa7a6e193522b
mmghtt <michellemarighetti1996@gmail.com>
2021-10-15 19:26:13 -0300

    Adicionar arquivos de projeto.

---
+++ ConsolePessoaClasse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePessoaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
