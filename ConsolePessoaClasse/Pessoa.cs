f080e946afefe5a9a719cd5a5054426df20d54d4
mmghtt <michellemarighetti1996@gmail.com>
2021-10-16 01:47:08 -0300

    primeira classe finalizada

---
+++ ConsolePessoaClasse/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePessoaClasse
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+        //apenas define como int qundo for fazer calculos com numeros
+        public string rg;
+        public string cpf;
+
+        public void Acenar(Pessoa pessoa)
+        {
+            Console.WriteLine("{0} acenou para {1}", this.nome, pessoa.nome);
+        }
+
+        public void PerguntaIdade (Pessoa pessoa)
+        {
+            Console.WriteLine("{0} qual sua idade? ", pessoa.nome);
+            pessoa.RespostaIdade();
+        }
+
+        public void RespostaIdade()
+        {
+            Console.WriteLine("Eu tenho {0} anos", this.idade);
+        }
+    }
+}
--- ConsolePessoaClasse/Program.cs
+++ ConsolePessoaClasse/Program.cs
@@ -6,7 +6,38 @@ namespace ConsolePessoaClasse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Michelle!");
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Digite o nome: ");
+            pessoa.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            pessoa.idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite seu RG: ");
+            pessoa.rg = Console.ReadLine();
+
+            Console.WriteLine("Digite sei CPF: ");
+            pessoa.cpf = Console.ReadLine();
+
+            Console.WriteLine("Nome: {0}", pessoa.nome);
+            Console.WriteLine("Idade: {0}", pessoa.idade);
+            Console.WriteLine("RG: {0}", pessoa.rg);
+            Console.WriteLine("CPF {0}", pessoa.cpf);
+
+            Pessoa pessoaDani = new Pessoa();
+            pessoaDani.nome = "Daniele Marighetti";
+            pessoaDani.idade = 32;
+            pessoaDani.rg = "12.000.00.-00";
+            pessoaDani.cpf = "123.000.000-00";
+
+            Console.WriteLine("Nome: {0}", pessoaDani.nome);
+            Console.WriteLine("Idade: {0}", pessoaDani.idade);
+            Console.WriteLine("RG: {0}", pessoaDani.rg);
+            Console.WriteLine("CPF {0}", pessoaDani.cpf);
+
+            pessoa.Acenar(pessoaDani);
+            pessoa.PerguntaIdade(pessoaDani);
         }
     }
 }
